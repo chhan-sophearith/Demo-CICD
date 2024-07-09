061a465bc0a8ae68b0db51ab9fd78864b1220c4b
chhan-sophearith <chhansophearithit@gmail.com>
2024-07-09 11:04:21 +0700

    init

---
+++ DemoCICD/DemoCICD/DemoCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCICDApp.swift
+//  DemoCICD
+//
+//  Created by Brilliant Dev on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
